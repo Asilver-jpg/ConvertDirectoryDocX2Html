b9baf6f6f8f0cb5ff02469bb76f3b577b04426e1
Asilver-JPG <asilver7493@gmail.com>
2023-03-29 15:56:50 -0400

    Working!

--- convertDocToHTML.py
+++ convertDocToHTML.py
@@ -1,20 +1,40 @@
 import mammoth
 import os
 import shutil
+import zipfile
 
+
+def ignore_files(dir, files):
+    return [f for f in files if os.path.isfile(os.path.join(dir, f))]
+ 
 def main():
     path = os.getcwd()
-    originalPath = "f{path}/test1"
-    destinationPath = "f{path}/test2"
-    shutil.rmtree(destinationPath)
+    print(path)
+    originalPath = f"{path}/test1"
+    destinationPath = f"{path}/test2"
+    copyDirectory(originalPath, destinationPath)
+    for subdir, dirs, files in os.walk(originalPath):
+        for file in files:
+            currFilePath =(os.path.join(subdir, file))
+            fileName = file.split('.')[0]
+            if(file):
+                createHtml(currFilePath, path, fileName)
 
-def createHtml(path, name):
-    f = open("C:\\Users\\asilv\\Code\\UNSHUT\\misc\\Doc2Html\\test.docx", 'rb')
-    b = open('filename.html', 'wb')
-    document = mammoth.convert_to_html(f)
-    b.write(document.value.encode('utf8'))
-    f.close()
-    b.close()
+def copyDirectory(src, dest):
+    if(os.path.exists(dest)):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest, ignore=ignore_files)
+
+def createHtml(textFilePath, path, name):
+    if(zipfile.is_zipfile(textFilePath)):
+        f = open(textFilePath, 'rb')
+        b = open(f"{path}/test2/{name}.html", 'wb')
+        document = mammoth.convert_to_html(f)
+        b.write(document.value.encode('utf8'))
+        f.close()
+        b.close()
+
+
 
 
 main()
